b63983386a7f1f6fd7afea3855c5f5b9bd571dab
rafaelsf80 <rafaelsf80@gmail.com>
2013-01-05 06:58:15 -0800

    Create commands.h

---
+++ commands.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2010 eralsaz rsfuente@it.uc3m.es
+ *
+ * commands.h
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef COMMANDS_H_
+#define COMMANDS_H_
+
+char *
+auth_command(char * nick_name, char * user_name, char * host_name, char * server_name, char * real_name);
+
+#endif /* COMMANDS_H_ */
