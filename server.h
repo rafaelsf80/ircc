86742f0a916474c7fe680d74c7e5d539599e17b9
rafaelsf80 <rafaelsf80@gmail.com>
2013-01-05 06:59:20 -0800

    Create server.h

---
+++ server.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2010 Rafael Sanchez rsfuente@it.uc3m.es
+ *
+ * server.h
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef SERVER_H_
+
+#define SERVER_H
+
+#endif /* SERVER_H_ */
